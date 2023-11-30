666311fe74acf2ef0509f64bef836d854ccfcaf1
stevensillsii <stevensills2@gmail.com>
2023-11-29 16:59:54 -0800

    cleaned some bugs up

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,6 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("Put to the database");
   const db = await initdb("jate", 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
